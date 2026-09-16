1cf121b1349657333a49d5fb60662bbe6b20f4a1
Bhargavbhardwaj <bhardwajbhargav944007@gmail.com>
2026-09-16 23:58:00 +0530

    stores current key and evolves it

--- key_manager.py
+++ key_manager.py
@@ -1,13 +1,11 @@
-import hashlib
 import secrets
+import hashlib
 
 
 class KeyManager:
+
     def __init__(self):
         self.current_key = secrets.token_bytes(32)
-
-    def get_current_key(self):
-        return self.current_key
 
     def evolve_key(self):
         old_key = self.current_key
@@ -16,7 +14,7 @@ class KeyManager:
             b"FORWARD_PRIVACY_KEY_EVOLUTION" + old_key
         ).digest()
 
-        # Remove our reference to the old key
-        del old_key
+        return self.current_key
 
+    def get_current_key(self):
         return self.current_key
